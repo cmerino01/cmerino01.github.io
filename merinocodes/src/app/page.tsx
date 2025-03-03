8ab0396e1f2229829a4d11725365520ca52c670f
Christopher Merino Brito <merinobr@usc.edu>
2025-03-02 18:52:21 -0800

    update src path for image

--- merinocodes/src/app/page.tsx
+++ merinocodes/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/cmerino01.github.io/globe.svg"
+            src="/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
