e9c4c16048f787f2ee90acb197b22ad7dba02cb1
Christopher Merino Brito <cmerinobr@gmail.com>
2025-03-02 21:04:57 -0800

    Update next.config.ts

--- merinocodes/next.config.ts
+++ merinocodes/next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-// const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
    /**
    * Enable static exports.
@@ -10,18 +8,6 @@ const nextConfig: NextConfig = {
    */
    output: "export",
 
-   /**
-    * Set base path. This is the slug of your GitHub repository.
-    *
-    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-    */
-/*    basePath: isProd ? "/nextjs-github-pages" : "",
-
-
-   assetPrefix: isProd ? "/nextjs-github-pages" : "", */
-
-   trailingSlash: true,
- 
    /**
     * Disable server-based image optimization. Next.js does not support
     * dynamic features with static exports.
