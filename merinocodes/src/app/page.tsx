45c0a4e41f5f380a785d865b1ba6ccdc2ffb5b1f
cmerino01 <cmerinobr@gmail.com>
2025-03-02 21:57:47 -0800

    first draft

--- merinocodes/src/app/page.tsx
+++ merinocodes/src/app/page.tsx
@@ -1,8 +1,60 @@
 import Image from "next/image";
+import React from "react";
+import * as NavMenu from "@/components/ui/navigation-menu";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <NavMenu.NavigationMenu>
+        <NavMenu.NavigationMenuList>
+          <NavMenu.NavigationMenuItem>
+            <NavMenu.NavigationMenuTrigger>
+              About me
+            </NavMenu.NavigationMenuTrigger>
+            <NavMenu.NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li>
+                  <a href="/about-me">My story</a>
+                </li>
+                <li>
+                  <a href="/skills">Education & Career</a>
+                </li>
+                <li>
+                  <a href="/resume.pdf">Resume</a>
+                </li>
+              </ul>
+            </NavMenu.NavigationMenuContent>
+          </NavMenu.NavigationMenuItem>
+          <NavMenu.NavigationMenuItem>
+            <NavMenu.NavigationMenuTrigger>
+              Projects
+            </NavMenu.NavigationMenuTrigger>
+            <NavMenu.NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li>
+                  <a href="">AI Fitness Trainer</a>
+                </li>
+                <li>
+                  <a href="">TBD</a>
+                </li>
+                <li>
+                  <a href="">TBD</a>
+                </li>
+              </ul>
+            </NavMenu.NavigationMenuContent>
+          </NavMenu.NavigationMenuItem>
+          <NavMenu.NavigationMenuItem>
+            <NavMenu.NavigationMenuTrigger>Other</NavMenu.NavigationMenuTrigger>
+            <NavMenu.NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li>
+                  <a href="">Coming soon...</a>
+                </li>
+              </ul>
+            </NavMenu.NavigationMenuContent>
+          </NavMenu.NavigationMenuItem>
+        </NavMenu.NavigationMenuList>
+      </NavMenu.NavigationMenu>
       <main className="flex flex-col gap-8 row-start-2 text-center">
         <h1>merinocodes.com</h1>
         <h2>new version coming soon...</h2>
---
+++ merinocodes/src/app/skills.tsx
@@ -0,0 +1,18 @@
+export default function Skills() {
+  return (
+    <>
+      <p>
+        I am a graduate of the University of Southern California with a Bachelor
+        of Science in Computer Engineering and CS. While at USC I gained some
+        experience in writing C++ , C, Java, Python, and JavaScript. I also have
+        experience with wire wrapping electronics, soldering, and using
+        oscilloscopes.
+      </p>
+      <p>
+        I am currently employed at Microsoft as a Software Engineer. I started
+        my role in August 2023 and have been working on the Power Policy team
+        within the Power Apps organization.
+      </p>
+    </>
+  );
+}
